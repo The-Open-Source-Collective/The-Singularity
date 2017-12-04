b9a9ee22bd5963c39f491e0939b858ab32c4cdfc
Branden Martin <self@brandenmartin.com>
2017-12-03 23:44:54 -0500

    Added IRC

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,18 @@
 import {Hooks} from "./Hooks";
+import {IRC} from "./Connections/IRC";
 
 export class Bot {
 
     constructor() {
         let hooks = new Hooks();
+        let irc = new IRC("irc.alphachat.net", 6667, "Alexa", "Alexa", "Alexa IRC AI");
+
+        irc.on("motd", () => {
+            irc.join("#ark");
+        });
+
+        irc.on("join", (channel: string, nick: string) => {
+        });
     }
 
 }
